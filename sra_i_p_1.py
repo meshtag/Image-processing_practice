975f5414e355ddc57deed71c4a7d1272ec74a3fd
meshtag <63031630+meshtag@users.noreply.github.com>
2020-09-04 02:32:12 +0530

    Delete sra_i_p_1.py

--- sra_i_p_1.py
+++
@@ -1,6 +0,0 @@
-import PIL
-from PIL import Image
-im=Image.open('rotate.png')
-i=int(input('Enter the desired angle of rotation for the image- '))
-im=im.rotate(i,PIL.Image.NEAREST,expand = 1)
-im.show()
