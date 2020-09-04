b6728c44d6cf2fc434be748a02f2ba7cb2c0f5f3
meshtag <63031630+meshtag@users.noreply.github.com>
2020-09-05 00:55:58 +0530

    Update rotating.py

--- rotating.py
+++ rotating.py
@@ -1,54 +1,53 @@
-import matplotlib.image as mimg
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt 
 import math
-im1=mimg.imread('rotate.png')
-orig_arr=np.array(im1)#Numpy array of original image. 
-x,y,c=im1.shape
-i=int(input('Enter the desired angle of rotation '))
-ans=np.empty((x,y,c))
+import numpy as np
+img=plt.imread('rotate.png')
+img_arr=np.array(img)
+i=int(input('Enter angle of rotation  '))
+fig=plt.figure()
+ax=plt.gca()
+i=math.radians(i)
+T=np.array([math.cos(i),math.sin(i),0,-math.sin(i),math.cos(i),0,0,0,1])#Rotation matrix for anticlockwise rotation
+T.resize(3,3)
+x,y,_=img_arr.shape
+print("Input image shape is :"+str(img_arr.shape))
+new_img=np.empty((x,y,_))
 for x1 in range(x):
     for y1 in range(y):
-            xr=abs(int(x1*math.cos(math.radians(i))-y1*math.sin(math.radians(i))))
-            yr=abs(int(x1*math.sin(math.radians(i))+y1*math.cos(math.radians(i))))
-            if(xr<x and yr<y):
-                ans[xr,yr,:] = orig_arr[x1,y1,:]
-                #print(str(orig_arr[x1,y1,:] )+"    "+str(ans[xr,yr,:]))
-rot_image=plt.imshow(ans)
-plt.show()
-
-T=np.array([math.cos(math.radians(i)),-math.sin(math.radians(i)),math.sin(math.radians(i)),math.cos(math.radians(i))])
-T.resize(2,2)
-#print(T)
-INV_T=np.linalg.inv(T)
-#print(INV_T)
-def neighbour_interpolation(x1,y1,im1,INV_T):
-    x_m,y_m,_=im1.shape
-    x_max=int(x_m)-1
-    y_max=int(y_m)-1
-    x,y,_ = INV_T @ np.array([x1,y1,1])
-    if(np.floor(x)==x and np.floor(y)==y):
-        return im1[x,y,]
-    if(abs(np.floor(x)-x)<abs(np.ceil(x)-x)):
-        x=int(x)
+        co_ord_mat=np.array([x1-img_arr.shape[0]/2,y1-img_arr.shape[1]/2,1])#Making appropriate changes in co-ordinates for rotating the image about its center.
+        tra_mat = co_ord_mat @ T
+        nx,ny,_=tra_mat
+        nx,ny=int(nx+img_arr.shape[0]/2),int(ny+img_arr.shape[1]/2)
+        if(nx<img_arr.shape[0] and ny<img_arr.shape[1]):
+            new_img[nx,ny,:]=img_arr[x1,y1,:]
+T_INV=np.linalg.inv(T)
+def interpolation(x1,y1,img_arr):#Function for implementing nearest neighbour interpolation method
+    co_ord_mat1=np.array([x1-img_arr.shape[0]/2,y1-img_arr.shape[1]/2,1])
+    tra_mat1 = co_ord_mat1 @ T_INV
+    nx1,ny1,_=tra_mat1
+    x1_max,y1_max=img_arr.shape[0]-1,img.shape[1]-1
+    nx1,ny1=int(nx1+img_arr.shape[0]/2),int(ny1+img_arr.shape[1]/2)
+    if(nx1==np.floor(nx1) and ny1==np.floor(ny1)):
+        if(nx1>x1_max):
+            nx1=x1_max
+        if(ny1>y1_max):
+            ny1=y1_max
+        return img_arr[nx1,ny1,]
+    if(abs(nx1-np.floor(nx1))<abs(nx1-np.ceil(nx1))):
+        nx1=np.floor(nx1)
     else:
-        x=int(x)+1
-    if(abs(np.floor(y)-y)<abs(np.ceil(y)-y)):
-        y=int(y)
+        nx1=np.ceil(nx1)
+    if(abs(ny1-np.floor(ny1))<abs(ny1-np.ceil(ny1))):
+        ny1=np.floor(ny1)
     else:
-        y=int(y)+1
-    if(x>x_max):
-        x=x_max
-    if(y>y_max):
-        y=y_max
-    return im1[x,y,]
+        ny1=np.ceil(ny1)
+    if(nx1>x1_max):
+        nx1=x1_max
+    if(ny1>y1_max):
+        ny1=y1_max
+    return img_arr[nx1,ny1,]
 for x1 in range(x):
     for y1 in range(y):
-        ans[x1,y1,:]=neighbour_interpolation(x1,y1,im1,INV_T)
-
-rot1=plt.imshow(ans)
+        new_img[x1,y1,:] = interpolation(x1,y1,img_arr)
+plt.imshow(new_img)
 plt.show()
-
-
-
-
